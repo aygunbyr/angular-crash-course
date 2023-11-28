910454e459c456cb7466cbca98039d224c600081
Aygün Bayır <turkoaygun@gmail.com>
2023-11-28 11:05:13 +0300

    service injection: constructor injection

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MyFirstService } from '../services/my-first.service';
 
 @Component({
   selector: 'app-my-first-comp',
@@ -12,9 +13,14 @@ export class MyFirstCompComponent {
   isSubmitted: boolean = false;
   messages: Array<any> = [];
 
+  constructor(private service: MyFirstService) {
+    this.messages = this.service.getAllMessages();
+    this.isSubmitted = this.messages.length > 0;
+  }
+
   onSubmit() {
     this.isSubmitted = true;
-    this.messages.push({
+    this.service.insert({
       name: this.name,
       email: this.email,
       message: this.message,
@@ -23,6 +29,6 @@ export class MyFirstCompComponent {
   }
 
   deleteMessage(index: number) {
-    this.messages.splice(index, 1);
+    this.service.deleteMessage(index);
   }
 }
